10df510f4c7b6267ec6bba5107d02134d100c494
Sergey Busygin <tranebus3@yandex.ru>
2020-09-15 11:16:52 +0300

    mistake

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -8,7 +8,7 @@ public class StreamMain {
         ArrayList<Integer> array = new ArrayList<Integer>(13);
         array.addAll(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
 
-        Stream<Integer> stream = array.stream()
+        array.stream()
                 .filter(x -> x > 0)
                 .filter(x -> x % 2 == 0)
                 .sorted(Comparator.naturalOrder())
